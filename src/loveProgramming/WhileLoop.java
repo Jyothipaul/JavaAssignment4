ced952be930b0028e51988e7bc1469f1cf1fe7c6
jyothipaul@yahoo.com <jyothipaul@yahoo.com>
2019-01-06 00:04:25 +0000

    Java Assignment 4

---
+++ src/loveProgramming/WhileLoop.java
@@ -0,0 +1,13 @@
+package loveProgramming;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+        while (count <= 100) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
